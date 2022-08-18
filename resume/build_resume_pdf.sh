ea22d6c53b5804683de6bfb6bdd25c8afe09df6e
Jan C. Brammer <jan.c.brammer@gmail.com>
2022-08-18 10:33:44 +0000

    Moved files for building resume to separate directory.

--- resume/build_resume_pdf.sh
+++ resume/build_resume_pdf.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Build html from Markdown (delete first three lines containing Jekyll header from index.md).
-pandoc -o resume.html -f gfm -s resume_head.md <(sed '1,3d' index.md) --metadata title="Jan C. Brammer"
+pandoc -o resume.html -f gfm -s resume_head.md <(sed '1,3d' ../index.md) --metadata title="Jan C. Brammer"
+
+# compile SCSS to SCC
+sass resume.scss resume.css
 
 # Build pdf from html styled with css.
 weasyprint resume.html resume.pdf -s resume.css
