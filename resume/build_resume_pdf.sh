a5921df53c5f9867a74de8938f44882d505af848
Jan C. Brammer <jan.c.brammer@gmail.com>
2024-04-16 14:38:01 +0000

    Configure "latest update"

--- resume/build_resume_pdf.sh
+++ resume/build_resume_pdf.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Build html from Markdown (delete first three lines containing Jekyll header from index.md).
+# Build HTML from Markdown (delete first three lines containing Jekyll header from index.md).
 pandoc -o resume.html -f gfm resume_head.md <(sed '1,3d' ../index.md)
 
-# compile SCSS to SCC
+# Compile SCSS to CSS.
 sass resume.scss resume.css
 
-# Build pdf from html styled with css.
+# Inject latest update into CSS.
+latest_update=$(grep latest_update: ../_config.yml | sed 's/latest_update: //' | tr -d '"') 
+sed -i "s/latest update: <latest_update>/latest update: $latest_update/" resume.css
+
+# Build PDF from HTML styled with CSS.
 weasyprint resume.html resume.pdf -s resume.css
